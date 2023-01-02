3206c572d8023772649feb74cb226f0711baf1be
martin.peychev97@gmail.com <martin.peychev97@gmail.com>
2023-01-02 16:48:18 +0200

    change

--- web-app/Controllers/QrCodeController.cs
+++ web-app/Controllers/QrCodeController.cs
@@ -10,12 +10,29 @@ namespace web_app.Controllers
 {
     public class QrCodeController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public string GenerateQRCode()
+        //public string GenerateQRCode()
+        //{
+        //    var callingUrl = Request.GetTypedHeaders().Referer.ToString();
+
+        //    using (MemoryStream ms = new MemoryStream())
+        //    {
+        //        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        //        QRCodeData qrCodeData = qrGenerator.CreateQrCode(callingUrl, QRCodeGenerator.ECCLevel.Q);
+        //        QRCode qrCode = new QRCode(qrCodeData);
+        //        Bitmap bitMap = qrCode.GetGraphic(20);
+        //        bitMap.Save(ms, ImageFormat.Png);
+        //        return "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+        //    }
+        //}
+        
+
+        [HttpPost]
+        public IActionResult Index(string qrcode)
         {
             var callingUrl = Request.GetTypedHeaders().Referer.ToString();
 
@@ -24,36 +41,14 @@ namespace web_app.Controllers
                 QRCodeGenerator qrGenerator = new QRCodeGenerator();
                 QRCodeData qrCodeData = qrGenerator.CreateQrCode(callingUrl, QRCodeGenerator.ECCLevel.Q);
                 QRCode qrCode = new QRCode(qrCodeData);
-                Bitmap bitMap = qrCode.GetGraphic(20);
-                bitMap.Save(ms, ImageFormat.Png);
-                return "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                using (Bitmap bitMap = qrCode.GetGraphic(20))
+                {
+                    bitMap.Save(ms, ImageFormat.Png);
+                    ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
             }
+
+            return View();
         }
-
-        //public IActionResult GenerateQrCode()
-        //{
-            
-        //    var callingUrl = Request.GetTypedHeaders().Referer.ToString();
-
-        //    using var qrgenerator = new QRCodeGenerator();
-        //    var qrCodeData = qrgenerator.CreateQrCode(callingUrl, QRCodeGenerator.ECCLevel.L);
-            
-        //    var qrCode = new PngByteQRCode(qrCodeData);
-        //    var qrCodeImage = qrCode.GetGraphic(20);
-        //    var output = Convert.ToBase64String(qrCodeImage);
-
-        //    return View(output);
-            
-        //}
-
-        //[NonAction]
-        //private static Byte[] BitmapToBytesCode(Bitmap image)
-        //{
-        //    using (MemoryStream stream = new MemoryStream())
-        //    {
-        //        image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-        //        return stream.ToArray();
-        //    }
-        //}
     }
 }
