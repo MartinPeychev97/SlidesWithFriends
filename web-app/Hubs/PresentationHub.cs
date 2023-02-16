45d25e177ee44421f8a6f0bff2ab8deeec3d5384
Kristian Ganchev <kris.ganchev91@gmail.com>
2023-02-16 16:50:13 +0200

    #41 showed joined users on first slide

--- web-app/Hubs/PresentationHub.cs
+++ web-app/Hubs/PresentationHub.cs
@@ -1,22 +1,49 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using DAL.EntityModels;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace web_app.Hubs
 {
     public class PresentationHub : Hub
     {
+        private static Dictionary<string, string> connectedUsers = new Dictionary<string, string>();
+
         public override async Task OnConnectedAsync()
         {
-            var httpContext = Context.GetHttpContext();
-            var presentationId = httpContext.Request.Query["presentationId"];
+            var presentationId = GetPresentationId();
+
             await Groups.AddToGroupAsync(Context.ConnectionId, presentationId);
         }
 
-        public Task UpdateSlide(int indexh, int indexv)
+        public async Task Join(string username, string image)
+        {
+            var presentationId = GetPresentationId();
+            connectedUsers.Add(username, image);
+            await Clients.Group(presentationId).SendAsync("UserJoined", connectedUsers);
+        }
+
+        public async Task Leave(string username)
+        {
+            var presentationId = GetPresentationId();
+            connectedUsers.Remove(username);
+            await Clients.Group(presentationId).SendAsync("UserLeft", connectedUsers);
+        }
+
+
+        public async Task UpdateSlide(int indexh, int indexv)
+        {
+            var presentationId = GetPresentationId();
+            await Clients.Group(presentationId).SendAsync("UpdateSlide", indexh, indexv);
+        }
+
+        private string GetPresentationId()
         {
             var httpContext = Context.GetHttpContext();
             var presentationId = httpContext.Request.Query["presentationId"];
-            return Clients.Group(presentationId).SendAsync("UpdateSlide", indexh, indexv);
+            return presentationId;
         }
     }
 }
--- web-app/wwwroot/signalr/presentation.js
+++ web-app/wwwroot/signalr/presentation.js
@@ -1,9 +1,37 @@
 ﻿let connection = new signalR.HubConnectionBuilder()
-    .withUrl(`/hubs/presentation?presentationId=${presentationId}`,)
+    .withUrl(`/hubs/presentation?presentationId=${presentationId}`)
     .build();
 
 connection.on("UpdateSlide", function (indexh, indexv) {
     Reveal.slide(indexh, indexv);
+});
+
+connection.on("UserJoined", function (users) {
+    $("#users-container").empty();
+    $.each(users, function (user, image) {
+        console.log(user)
+        console.log(image)
+        $("#users-container").append(`
+                    <div class="avatar">
+                        <img src="${image}" />
+                        <p>${user}</p>
+                    </div>
+`);
+    });
+});
+
+connection.on("UserLeft", function (users) {
+    $("#users-container").empty();
+    $.each(users, function (user, image) {
+        console.log(user)
+        console.log(image)
+        $("#users-container").append(`
+                    <div class="avatar">
+                        <img src="${image}" />
+                        <p>${user}</p>
+                    </div>
+`);
+    });
 });
 
 connection.start().then(function () {
@@ -15,6 +43,12 @@ connection.start().then(function () {
                 return console.error(err.toString());
             });
     });
+
+    connection.invoke("Join", username, image);
 }).catch(function (err) {
     return console.error(err.toString());
+});
+
+window.addEventListener("beforeunload", function () {
+    connection.invoke("Leave", username);
 });
